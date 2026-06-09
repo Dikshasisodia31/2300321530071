a4c23073216c55d2e1258e89d1d6012a1a5feca3
Dikshasisodia31 <dikshasisodia3112@gmail.com>
2026-06-09 12:36:12 +0530

    Add folders

---
+++ my-app/src/loggin_middleware/loggin_middleware.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loggin_middleware = () => {
+  return (
+    <div>
+       
+    </div>
+  )
+}
+
+export default loggin_middleware
---
+++ my-app/src/notification_system_design_be/notification_system_design_be.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const notification = [
+        {
+            "id" : "134jfnjavfn",
+            "type" : "Placement",
+            "message": "New hiring",
+            "timestamp": "09-06-2026",
+        },
+        {
+            "id" : "134jfnjavfn",
+            "type" : "event",
+            "message": "New hiring",
+            "timestamp": "09-06-2026",
+        },
+        {
+            "id" : "134jfnjavfn",
+            "type" : "Placement",
+            "message": "New hiring",
+            "timestamp": "09-06-2026",
+        },
+        {
+            "id" : "134jfnjavfn",
+            "type" : "Placement",
+            "message": "New hiring",
+            "timestamp": "09-06-2026",
+        },
+        {
+            "id" : "134jfnjavfn",
+            "type" : "event",
+            "message": "New hiring",
+            "timestamp": "09-06-2026",
+        }
+]
+
+export default notification
